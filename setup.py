991c4b054bccc4bd2d0b8c8f1b4ddf7355819b63
kkAyataka <kk.ayataka@gmail.com>
2020-03-07 22:16:08 +0900

    Update

--- setup.py
+++ setup.py
@@ -2,24 +2,21 @@
 
 from setuptools import setup, find_packages
 
-long_desc = '''
-This package contains the xlsxtable Sphinx extension.
-
-.. add description here ..
-'''
-
-requires = ['Sphinx>=0.6']
+long_desc = ''
+with open("README.rst", "r") as fh:
+    long_desc = fh.read()
 
 setup(
     name='sphinxcontrib-xlsxtable',
-    version='0.1',
+    version='0.1.7',
     url='https://github.com/kkAyataka/sphinxcontrib-xlsxtable',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-xlsxtable',
     license='MIT',
     author='kkAyataka',
     author_email='kk.ayataka@gmail.com',
-    description='Sphinx "xlsxtable" extension',
+    description='A sphinx extension for making table from Excel file',
     long_description=long_desc,
+    long_description_content_type='text/x-rst',
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -30,13 +27,15 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Sphinx :: Extension',
-        #'Framework :: Sphinx :: Theme',
         'Topic :: Documentation',
         'Topic :: Utilities',
     ],
     platforms='any',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=requires,
+    install_requires=[
+        'Sphinx',
+        'openpyxl'
+    ],
     namespace_packages=['sphinxcontrib'],
 )

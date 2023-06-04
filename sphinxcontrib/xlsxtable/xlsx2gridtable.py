3ea30549a040f87bd8f3671fb7f26ce2f759d542
kkAyataka <kk.ayataka@gmail.com>
2023-06-04 10:55:01 +0900

    Support embedded images

--- sphinxcontrib/xlsxtable/xlsx2gridtable.py
+++ sphinxcontrib/xlsxtable/xlsx2gridtable.py
@@ -1,6 +1,8 @@
 import argparse
+import os
 import unicodedata
 from openpyxl import load_workbook
+from .workbook.workbook import Workbook as ImageWorkbook
 
 def parse_index_str(index_str: str) -> int:
     """ Gets index number form Excel column letter (1-based).
@@ -72,12 +74,15 @@ class TableCell:
     is_merged_left = False
 
     def __init__(self, row, column, value):
+        self.row = row
+        self.column = column
+        self.set_value(value)
+
+    def set_value(self, value: str):
         values = []
         if value != None:
             values = f'{value}'.split('\n')
 
-        self.row = row
-        self.column = column
         self.values = values
         self.line_count = max(count_line(values), 1)
         self.width = get_max_width(values)
@@ -116,7 +121,8 @@ def get_rule(colmuns, is_head=False, is_end=False, is_top=False):
     return line_str
 
 def gen_reST_grid_table_lines(
-    filename: str,
+    file: str,
+    fullpath: str,
     header_rows=0,
     sheetname=None,
     start_row=1,
@@ -125,9 +131,14 @@ def gen_reST_grid_table_lines(
     exclude_rows=None,
     include_columns=None,
     exclude_columns=None):
+    """
+    :param file: The :file: parameter value in the directive
+    :param fullpath: The full path for the :file: parameter file
+    """
 
+    # Parse workbook
     wb = load_workbook(
-        filename=filename,
+        filename=fullpath,
         read_only=False, # Can not get merged cell information if read_only is True
         keep_vba=False,
         data_only=True,
@@ -140,26 +151,57 @@ def gen_reST_grid_table_lines(
     except:
         ws = wb.active
 
-    # rows / columns
-    offset_row = max(ws.min_row, start_row)
-    offset_col = max(ws.min_column, start_column)
+    # Parse for images
+    img_wb = ImageWorkbook.parse(fullpath)
+    img_ws = img_wb.get_sheet(ws.title)
+    img_min_from_row = img_ws.drawing.min_from_row + 1 if img_ws.drawing != None else 1048576
+    img_max_from_row = img_ws.drawing.max_from_row + 1 if img_ws.drawing != None else 1
+    img_min_from_col = img_ws.drawing.min_from_col + 1 if img_ws.drawing != None else 16384
+    img_max_from_col = img_ws.drawing.max_from_col + 1 if img_ws.drawing != None else 1
 
-    use_rows = get_use_indexes(ws.min_row, ws.max_row, include_rows, exclude_rows)
-    use_cols = get_use_indexes(ws.min_column, ws.max_column, include_columns, exclude_columns)
+    # rows / columns
+    min_row = min(ws.min_row, img_min_from_row)
+    max_row = max(ws.max_row, img_max_from_row)
+    min_col = min(ws.min_column, img_min_from_col)
+    max_col = max(ws.max_column, img_max_from_col)
+    offset_row = max(min_row, start_row)
+    offset_col = max(min_col, start_column)
+
+    use_rows = get_use_indexes(min_row, max_row, include_rows, exclude_rows)
+    use_cols = get_use_indexes(min_col, max_col, include_columns, exclude_columns)
 
     # parse cell info
     table_cells = []
-    for r in range(offset_row, ws.max_row + 1):
+    for r in range(offset_row, max_row + 1):
         if r in use_rows:
             # appebd array for row
             table_cells.append([])
 
             # get line count in the row
             r_index = len(table_cells) - 1
-            for c  in range(offset_col, ws.max_column + 1):
+            for c in range(offset_col, max_col + 1):
                 if c in use_cols:
                     tc = TableCell(r, c, ws.cell(r, c).value)
                     table_cells[r_index].append(tc)
+
+    # Handles images, embeds image directive
+    if img_ws.drawing != None:
+        # Make images directory from xlsx file
+        base_dir = os.path.dirname(fullpath)
+        dir_name = os.path.basename(fullpath) + '.media'
+        img_dir = os.path.join(base_dir, dir_name)
+        if os.path.exists(img_dir) == False:
+            os.mkdir(img_dir)
+        # Write out image files
+        for m in img_wb.media:
+            file_path = os.path.join(img_dir, os.path.basename(m))
+            with open(file_path, 'wb') as fs:
+                fs.write(img_wb.media[m])
+        # Add image directives in cells
+        for anchor in img_ws.drawing.two_cell_anchors:
+            r = anchor.from_pt.row - (offset_row - 1)
+            c = anchor.from_pt.col - (offset_col - 1)
+            table_cells[r][c].set_value(f'.. image:: {file}.media/{os.path.basename(anchor.image_path)}')
 
     # adjust line count
     row_count = len(table_cells)
@@ -224,10 +266,10 @@ def gen_reST_grid_table_lines(
     return grid_table_lines
 
 def draw_reST_grid_table(
-        filename, header_rows, sheet, start_row, start_column,
+        file, fullpath, header_rows, sheet, start_row, start_column,
         include_rows, exclude_rows,
         include_columns, exclude_columns):
-    lines = gen_reST_grid_table_lines(filename, header_rows, sheet, start_row, start_column,
+    lines = gen_reST_grid_table_lines(file, fullpath, header_rows, sheet, start_row, start_column,
         include_rows, exclude_rows, include_columns, exclude_columns)
 
     text ='\n'.join(lines)
@@ -248,7 +290,7 @@ def main():
 
     args = p.parse_args()
 
-    draw_reST_grid_table(args.file, args.header_rows, args.sheet,
+    draw_reST_grid_table(args.file, args.file, args.header_rows, args.sheet,
         args.start_row, args.start_column,
         args.include_rows, args.exclude_rows,
         args.include_columns, args.exclude_columns)
